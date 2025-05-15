a2b3d3878ee5be70b4a1f4abccbfe4eb78a81ffe
Callie Higgins <155601818+CallieHiggins7@users.noreply.github.com>
2025-05-14 22:39:10 -0400

    Update project.py

--- project.py
+++ project.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import expon
 
+#Make list of states to be added to the drop down menu
 states = [
     "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado",
     "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho",
@@ -17,29 +18,29 @@ states = [
     "Washington", "West Virginia", "Wisconsin", "Wyoming"
 ]
 
+#Make option box
 doesoption = st.selectbox("Which state would you like to select?", [""] + states)
-
+#Use selection to make sure the code runs after the user picks the state
 if doesoption:
     st.write("You selected:", doesoption)
-    #Reading csv file 
+#Reading csv file 
     df = pd.read_csv("ilidata.csv")
-
+#Changing the date from release date to weeks
     df["release_date"] = pd.to_datetime(df["release_date"])
     df["release_week"] = df["release_date"].dt.isocalendar().week
     df["release_year"] = df["release_date"].dt.isocalendar().year
     df["Weeks"] = ((df["release_date"] - df["release_date"].min()).dt.days) // 7
 
-    # Streamlit Header
+#Add title
     st.title("Influenza-like Illness Trends Over Time")
 
-    #Making time series
+#Making time series graph with a label and description
     st.header("Time Series of ILI Percent By Week")
     st.write("""This plot shows the percentage of influenza-like illness and how it has changed week-by-week. Every point on the graph represents the percent of ILI cases
-    observed in a given week.""")
+    observed in a given week. The graph shows us that about every year (roughly 52 weeks) the percentage of influenza like illness spikes then drops shortly after. This is important for public health officials and researchers to be
+    able to visualize because it allows them to predict and prepare for when influenza like illness will spike again in the future.""")
     #Making a specific dataframe with plotting data to make it easier to plot
     plotdata = df[['Weeks', 'ili']].rename(columns={'ili': 'Percent ILI'})
-
-    #Making time series plot
     st.line_chart(
     plotdata,
     x='Weeks',
@@ -47,22 +48,23 @@ if doesoption:
     color=['#FF5743']
 )
 
-    #Making histogram 
+#Histogram labels and description
     st.header("Distribution of ILI Percent with Exponential Fit")
     st.write("""
-    This histogram shows the distribution of ILI percent values. We then overlaid the estimate (1/lambda) from the exponential density function to look at
-    if the ILI values match the exponential distribution. Assuming each ILI value was independently drawn from the same exponential distribution,
-    the same mean provides a reasonable estimate of the true parameter, lambda, of the distribution.The density curve helps visualize how likely certain ILI percentages are, given this assumption.
+    This histogram shows the distribution of ILI percent values. The estimate (1/lambda) from the exponential density function was then overlaid to look at
+    if the ILI values match the exponential distribution. If it is assumed each ILI value was independently drawn from the same exponential distribution,
+    the same mean provides a reasonable estimate of the true parameter, lambda, of the distribution.
     """)
 
-    #Defining estimate
+#Defining the estimate
     ili_data = df["ili"]
     estimate = 1 / ili_data.mean()
 
+#Making the histogram
     fig, ax = plt.subplots()
     count, bins, _ = ax.hist(ili_data, bins=30, density=True, alpha=0.6, color="skyblue", label="ILI histogram")
 
-    #Adding exponential distribution
+#Adding exponential distribution overlaid
     x_values = np.linspace(0, ili_data.max(), 1000)
     ax.plot(x_values, expon.pdf(x_values, scale=estimate), 'r-', lw=2, label=f"Exponential(λ̂={estimate:.2f})")
     ax.set_title("Histogram of ILI Percent with Exponential Fit")
